f6e3d38d279f7b53357c4f84276db499e6dca181
Wang <quanglun2002@gmail.com>
2023-06-17 00:33:49 +0700

    EX16

---
+++ redux-demo/src/components/AddUser.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
+import { addUser } from '../features/Users';
+const AddUser = () => {
+    const dispatch = useDispatch();
+    const [name, setName] = useState('');
+    const [username, setUsername] = useState('');
+    return (
+        <div className='container card border-primary'>
+            <Form className='mt-3'>
+                <Form.Group className="mb-3" controlId="name">
+                    <Form.Control type="text" placeholder="Name"
+                        value={name} onChange={(event) => {
+                            setName(event.target.value)
+                        }} />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="username">
+                    <Form.Control type="text" placeholder="Username" value={username} onChange={(event) => {
+                        setUsername(event.target.value)
+                    }} />
+                </Form.Group>
+                <Button variant='primary' className='mb-3' style={{ width: '100%' }} onClick={() => {
+                    dispatch(addUser({ id: 0, name: name, username: username }))
+                }}>ADD USER</Button>
+            </Form>
+        </div>
+    );
+};
+
+export default AddUser;
---
+++ redux-demo/src/index.js
@@ -0,0 +1,26 @@
+import { configureStore } from '@reduxjs/toolkit';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App from './App';
+import userReducer from './features/Users';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+
+const store = configureStore({
+  reducer: {
+    users: userReducer,
+  },
+}); const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
